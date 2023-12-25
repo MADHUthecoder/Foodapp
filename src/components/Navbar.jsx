6597ef54624067e9a52d8d13c59b3e43441d67e7
KaushikKC <kccreations1704@gmail.com>
2023-12-25 16:00:15 +0530

    Added popup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {AiOutlineMenu, AiOutlineSearch} from "react-icons/ai"
 import { CgProfile } from "react-icons/cg";
 import { Link } from 'react-router-dom';
@@ -6,9 +6,27 @@ import { TbFilterStar } from "react-icons/tb";
 
 
 function Navbar() {
+  const [filterPop, setFilterPop] = useState(false);
   return (
     <>
+    {
+      filterPop &&
+      <div className='bg-slate-300/50 w-full h-full fixed top-0 left-0 flex items-center justify-center'>
+        <div className='w-[600px] h-[277px] bg-slate-400 flex flex-col items-center justify-center'>
+        <div className='flex'>
+        <label className='text-white' htmlFor="">Loaction</label>
+        <select name="location" placeholder="Loaction" id="">
+          <option value="">Chennai</option>
+          <option value="">Salem</option>
+          <option value="">Pondy</option>
+        </select>
+        </div>
+        </div>
+        
+      </div>
+    }
     <div className='mx-auto flex justify-between items-center p-4 bg-white '>
+
       <div className='flex items-center'>
       <AiOutlineMenu size={20} />
         <Link to='/' className='text-3xl px-10'>
@@ -21,7 +39,7 @@ function Navbar() {
       </div>
   
       <div className='flex w-[350px] justify-evenly'>
-        <div><TbFilterStar size={30}/></div>
+        <div onClick={() => setFilterPop(true)}><TbFilterStar size={30}/></div>
         <input type='text' placeholder='Enter food' className='bg-gray-100 rounded-xl drop-shadow-md px-2'></input>
         <AiOutlineSearch size={30} className='justify-center'/>
         <CgProfile size={30}/>
